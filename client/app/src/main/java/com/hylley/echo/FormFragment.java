42eb032fed385ce4aa7859a2fdd24e6fcf0e4538
Hydley F <hydley.jailsonk@gmail.com>
2023-10-26 17:04:38 -0300

    Finish client app tabs implementation
    Still lacks integration tho.

---
+++ client/app/src/main/java/com/hylley/echo/FormFragment.java
@@ -0,0 +1,18 @@
+package com.hylley.echo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FormFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_form, container, false);
+    }
+}
--- client/app/src/main/java/com/hylley/echo/MainActivity.java
+++ client/app/src/main/java/com/hylley/echo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.hylley.echo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.text.Normalizer;
 
 public class MainActivity extends AppCompatActivity
 {
-    Client client = new Client("girlhood04");
+    //region App
+    BottomNavigationView view;
 
+    FormFragment form_fragment = new FormFragment();
+    ChatFragment chat_fragment = new ChatFragment();
+
+    BadgeDrawable badge;
+    //enregion
+
+    //region Network
+    Client client = new Client("girlhood04");
     public static final boolean debug = true;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,14 +33,50 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, form_fragment).commit();
+        view = findViewById(R.id.navbar);
+        badge = view.getOrCreateBadge(R.id.chat);
+
+        view.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int selected_id = item.getItemId();
+
+                if(selected_id == R.id.form)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, form_fragment).commit();
+                    return true;
+                }
+
+                if(selected_id == R.id.chat)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, chat_fragment).commit();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
         client.start();
+    }
+
+    public void set_chat_icon_unread_badge(boolean visible)
+    {
+        badge.setVisible(visible);
+    }
+
+    public void set_chat_icon_unread_badge(boolean visible, int pops)
+    {
+        badge.setNumber(pops);
+        badge.setVisible(visible);
     }
 
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
-
         if(!isFinishing() || client == null) return;
 
         client.end_process();
