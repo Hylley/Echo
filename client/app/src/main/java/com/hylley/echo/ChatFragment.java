cd22f0845c00e850a45d76e50b40a96cec682055
Hydley F <hydley.jailsonk@gmail.com>
2023-10-27 12:10:44 -0300

    Chat UI design mostly finish
    Yet to figure out how to work with RecyclerView, tho.

--- client/app/src/main/java/com/hylley/echo/ChatFragment.java
+++ client/app/src/main/java/com/hylley/echo/ChatFragment.java
@@ -1,26 +1,38 @@
 package com.hylley.echo;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ScrollView;
-
-import org.jetbrains.annotations.NotNull;
 
 public class ChatFragment extends Fragment
 {
-    ScrollView scroll_view;
+    RecyclerView recycler_view;
+    EditText message_input;
 
     @Override
-    public void onViewCreated(@NonNull View view, @NotNull Bundle savedInstanceState)
+    @SuppressLint("ClickableViewAccessibility")
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
     {
-        scroll_view = getView().findViewById(R.id.chat_scroll_view);
-        scroll_view.post(() -> scroll_view.scrollTo(0, scroll_view.getBottom()));
+        recycler_view = view.findViewById(R.id.recycler_view);
+        message_input = view.findViewById(R.id.message_input);
+
+        recycler_view.post(() -> recycler_view.scrollTo(0, recycler_view.getBottom()));
+        message_input.setOnTouchListener((_view, motion) ->
+        {
+            recycler_view.scrollTo(0, recycler_view.getBottom());
+            return false;
+        });
+
+
     }
 
     @Override
